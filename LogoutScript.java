73bd8c3989a28fb44381e6bb7d4fc9d103e9b624
Akashb100 <103985825+Akashb100@users.noreply.github.com>
2022-06-01 21:55:33 +0530

    Update LogoutScript.java

--- LogoutScript.java
+++ LogoutScript.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.interactions.Actions;
 
 public class LogoutScript {
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver","chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.urbanladder.com/");
